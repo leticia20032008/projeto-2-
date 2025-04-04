85a2f58a7f2a8d7d5f5a373af082ae6a62d607a0
leticia20032008 <leticia.carvalho.oliveira20@escola.pr.gov.br>
2025-04-04 09:24:57 -0300

    04/04/2025

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-31T00:00:00");
-const tempoObjetivo2 = new Date("2025-10-30T00:00:00");
-const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-10-10T00:00:00");
+const tempoObjetivo2 = new Date("2025-11-30T00:00:00");
+const tempoObjetivo3 = new Date("2025-10-23T00:00:00");
+const tempoObjetivo4 = new Date("2025-08-10T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
